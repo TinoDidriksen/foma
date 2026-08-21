TinoDidriksen/foma

commit ef38e5263d8562dba2db353329729bc742ce2d02

Link flookup with ws2_32 on Windows

foma/CMakeLists.txt
@@ -197,3 +197,7 @@ else()
 	install(FILES "${CMAKE_CURRENT_BINARY_DIR}/libfoma.pc" DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig/")
 
 endif()
+
+if(WIN32)
+  target_link_libraries(flookup PRIVATE ws2_32)
+endif()

commit 718c60facf63b397cbf4a518b78d02b20c5504c8

Fix build broken by ef38e5263d8562dba2db353329729bc742ce2d02; Enable tests

foma/CMakeLists.txt
@@ -187,11 +187,12 @@ else()
     target_link_directories(foma-bin PRIVATE ${READLINE_LIBRARY_DIRS})
     set_target_properties(foma-bin PROPERTIES RUNTIME_OUTPUT_NAME foma)
 
-	if(MSYS OR NOT WIN32)
-		add_executable(flookup flookup.c)
-		target_link_libraries(flookup PRIVATE foma-static ${GETOPT_LIB})
-		install(TARGETS flookup RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+	add_executable(flookup flookup.c)
+	target_link_libraries(flookup PRIVATE foma-static ${GETOPT_LIB})
+	if(WIN32)
+		target_link_libraries(flookup PRIVATE ws2_32)
 	endif()
+	install(TARGETS flookup RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
 
 	add_executable(cgflookup cgflookup.c)
 	target_link_libraries(cgflookup PRIVATE foma-static ${GETOPT_LIB})
@@ -209,8 +210,6 @@ else()
 	install(TARGETS foma-bin cgflookup RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
 	install(FILES "${CMAKE_CURRENT_BINARY_DIR}/libfoma.pc" DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig/")
 
-endif()
-
-if(WIN32 AND TARGET flookup)
-	target_link_libraries(flookup PRIVATE ws2_32)
+	enable_testing()
+	add_test(NAME t_run COMMAND ./run.sh "$<TARGET_FILE:foma-bin>" WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/tests")
 endif()

foma/tests/run.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -ex
 
-foma -q -f test-leaky-redefine.foma || exit 1;
-foma -q -f test-segfault-eliminate.foma || exit 1;
-if ! foma -q -f test-error-rendering.foma  2>&1 | grep -q 'syntax error'
+FOMA=foma
+if [ $# -gt 0 ]; then
+  FOMA="$1"
+fi
+
+"$FOMA" -q -f test-leaky-redefine.foma || exit 1;
+"$FOMA" -q -f test-segfault-eliminate.foma || exit 1;
+if ! "$FOMA" -q -f test-error-rendering.foma  2>&1 | grep -q 'syntax error'
 then
   exit 1
 fi
-foma -q -f test-segfault-long-name > /dev/null || exit 1
-foma -q -f test-segfault-empty-fst.foma > /dev/null || exit 1;
-foma -q -f test-leaky-test.foma > /dev/null || exit 1;
+"$FOMA" -q -f test-segfault-long-name > /dev/null || exit 1
+"$FOMA" -q -f test-segfault-empty-fst.foma > /dev/null || exit 1;
+"$FOMA" -q -f test-leaky-test.foma > /dev/null || exit 1;
718c60facf63b397cbf4a518b78d02b20c5504c8
Tino Didriksen <mail@tinodidriksen.com>
2026-06-24 16:45:17 +0200

    Fix build broken by ef38e5263d8562dba2db353329729bc742ce2d02; Enable tests

--- foma/tests/run.sh
+++ foma/tests/run.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -ex
 
-foma -q -f test-leaky-redefine.foma || exit 1;
-foma -q -f test-segfault-eliminate.foma || exit 1;
-if ! foma -q -f test-error-rendering.foma  2>&1 | grep -q 'syntax error'
+FOMA=foma
+if [ $# -gt 0 ]; then
+  FOMA="$1"
+fi
+
+"$FOMA" -q -f test-leaky-redefine.foma || exit 1;
+"$FOMA" -q -f test-segfault-eliminate.foma || exit 1;
+if ! "$FOMA" -q -f test-error-rendering.foma  2>&1 | grep -q 'syntax error'
 then
   exit 1
 fi
-foma -q -f test-segfault-long-name > /dev/null || exit 1
-foma -q -f test-segfault-empty-fst.foma > /dev/null || exit 1;
-foma -q -f test-leaky-test.foma > /dev/null || exit 1;
+"$FOMA" -q -f test-segfault-long-name > /dev/null || exit 1
+"$FOMA" -q -f test-segfault-empty-fst.foma > /dev/null || exit 1;
+"$FOMA" -q -f test-leaky-test.foma > /dev/null || exit 1;
